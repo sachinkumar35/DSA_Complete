2d63c62b155da9bfe3dc87e683eb80f41555f5bd
PC <PC@DESKTOP-JU45RM1>
2022-07-24 21:58:17 +0530

    K-th element find

---
+++ DSA/src/practice/Kth_Element_1.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Arrays;
+
+public class Kth_Element_1 {
+
+	public static int kth(int arr[], int k)
+	{
+		Arrays.sort(arr);
+		return arr[k-1];
+	}
+	public static void main(String[] args) {
+		int arr[] = {30,20,5,10,8};
+		System.out.println(kth(arr, 4));
+		
+	}
+}
